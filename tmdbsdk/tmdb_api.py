383632b80031dc2ff185dc903d73e6e7e8e8b7c0
Josh Odell <j01101111sh@gmail.com>
2022-06-11 14:22:29 -0500

    Added RestAdapter to api interface

--- tmdbsdk/tmdb_api.py
+++ tmdbsdk/tmdb_api.py
@@ -1,3 +1,12 @@
+import logging
+
+from .tools import RestAdapter
+
+
 class TmdbApi:
-    def __init__(self):
-        pass
+    def __init__(self, api_key: str, api_ver: int = 3, safe_logging: bool = True):
+        supported_api_versions = {3}
+        if api_ver not in supported_api_versions:
+            logging.warning(f'Untested API version - API:{api_ver}')
+        self._rest_adapter = RestAdapter(
+            api_key=api_key, api_ver=api_ver, safe_logging=safe_logging)
